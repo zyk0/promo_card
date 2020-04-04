87e734a8b6273db3818f5d88fca2e2ee8ac8a5f9
zyk0 <77777mill@gmail.com>
2020-04-04 11:39:29 +0500

    first commit

---
+++ toltip.js
@@ -0,0 +1,9 @@
+﻿tippy('#tooltip', {
+  content: "При выполнении условий, которые указаны в правилах обслуживания",
+  placement: "bottom"
+});
+
+tippy('#age', {
+  content: "Возраст держателя карты от 18 до 39",
+  placement: "right"
+});
